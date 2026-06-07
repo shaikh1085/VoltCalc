f0e0e063fc71fdb8abcd8d394d017538185444fb
shaikh1085 <hr144329@gmail.com>
2026-06-07 13:57:14 +0500

    Update calculatorData.ts

--- src/data/calculatorData.ts
+++ src/data/calculatorData.ts
@@ -549,7 +549,7 @@ export const COUNTRY_CONFIGS: Record<SelectedCountry, CountryConfig> = {
     evEfficiencyUnits: 'miles/kWh',
     defaultAnnualDistance: 12000,
   // Line 551 ko badal kar ye kar dein:
-defaultGasPrice: 3.60, // Ab ye direct Price Per Gallon ban gaya hai
+defaultGasPrice: 4.21, // Ab ye direct Price Per Gallon ban gaya hai
     defaultHomeChargingRate: 0.17,
     defaultPublicChargingRate: 0.45,
     defaultTaxBracket: 0.22,
